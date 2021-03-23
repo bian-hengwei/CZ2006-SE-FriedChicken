bfc76ee985c0295b91c40ab4689e95bbe49c5c79
Fu Yongding <AsherFYD@gmail.com>
2021-03-23 23:25:13 +0800

    added javadoc for MapFragment.java

--- app/src/main/java/com/ntu/medcheck/view/fragment/MapFragment.java
+++ app/src/main/java/com/ntu/medcheck/view/fragment/MapFragment.java
@@ -9,25 +9,46 @@ import com.ntu.medcheck.R;
 
 public class mapFragment_default extends Fragment /*implements OnMapReadyCallback*/ {
 
-    // Required empty public constructor
+    /**
+     * Constructor for the fragment
+     */
     public mapFragment_default() {
     }
 
-    //function to display fragment
+    /**
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return View
+     * This function is called to inflate a view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
     }
 
     //functions needed to display map
-    /*@Override
+
+    /**
+     *
+     * @param view
+     * @param savedInstanceState
+     * This function is called after onCreateView to ensure that the fragment's root view is non-null
+     */
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     }
 
 
+    /**
+     *
+     * @param googleMap
+     * This function is triggered when the map is ready to be used
+     */
     @Override
     public void onMapReady(GoogleMap googleMap){
-    }*/
+    }
 
 }
 
