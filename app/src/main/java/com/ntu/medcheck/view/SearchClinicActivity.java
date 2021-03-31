752ff4a3a8072714ceb6fac6ca3ebda8cbc15d01
yinjiarui27 <62418592+yinjiarui27@users.noreply.github.com>
2021-04-01 01:12:40 +0800

    added some UI

--- app/src/main/java/com/ntu/medcheck/view/SearchClinicActivity.java
+++ app/src/main/java/com/ntu/medcheck/view/SearchClinicActivity.java
@@ -3,6 +3,11 @@ package com.ntu.medcheck.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +19,26 @@ import com.ntu.medcheck.R;
  * When user clicks on a clinic, its information will be shown below the map
  * The class calls AddCheckupActivity if user wants to add a clinic to checkup schedule
  */
-public class SearchClinicActivity extends AppCompatActivity {
+public class SearchClinicActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinic_map);
+
+        Spinner typeList = findViewById(R.id.typeList);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.checkupList, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeList.setAdapter(adapter);
+        typeList.setOnItemSelectedListener(this);
+
+        Button addToScheduleButton = findViewById(R.id.addToSchedule);
+        addToScheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SearchClinicActivity.this, AddCheckupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     public void enterCheckupType(View v){
@@ -38,12 +58,15 @@ public class SearchClinicActivity extends AppCompatActivity {
         //display clinics on map
     }
 
-    public void addToSchedule(View v){
-        //add the clinic to a new checkup schedule
-        Intent i = new Intent(this, AddCheckupActivity.class);
-        startActivity(i);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String types = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),types, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
+    }
 }
 
