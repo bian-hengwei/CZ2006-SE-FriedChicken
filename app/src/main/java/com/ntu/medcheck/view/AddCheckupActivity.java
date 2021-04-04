8fa9a2c32c3e8ca4b6efe10b3361598640c0ebb4
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2021-04-04 23:03:01 +0800

    add checkup done

--- app/src/main/java/com/ntu/medcheck/view/AddCheckupActivity.java
+++ app/src/main/java/com/ntu/medcheck/view/AddCheckupActivity.java
@@ -1,17 +1,19 @@
 package com.ntu.medcheck.view;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ntu.medcheck.R;
+import com.ntu.medcheck.controller.CheckUpMgr;
 import com.ntu.medcheck.utils.SafeOnClickListener;
 import com.ntu.medcheck.view.fragment.CheckupFragment;
 
@@ -29,27 +31,16 @@ public class AddCheckupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_checkup);
 
-
         checkup_type = findViewById(R.id.confirmed_checkup_type); //get the textview
         String setCheckUpType = getIntent().getStringExtra("type of checkup"); //get the string value from the intent
         checkup_type.setText(setCheckUpType); //set checkup type
 
         clinic_name = findViewById(R.id.confirmed_clinic_name); //get the textview
         String setClinicName = getIntent().getStringExtra("Clinic name set"); //get the string value from the intent
-        clinic_name.setText(setClinicName); //set clinic name
+        clinic_name.setText(setClinicName);
 
-        /*FloatingActionButton addButton1 = findViewById(R.id.editType);
-        addButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onOneClick(View v) {
-                Intent i = new Intent(AddCheckupActivity.this, SearchClinicActivity.class);
-                startActivity(i);
-            }
-
-        });*/
-
-        FloatingActionButton addButton2 = findViewById(R.id.editClinic);
-        addButton2.setOnClickListener(new SafeOnClickListener() {
+        FloatingActionButton addButton = findViewById(R.id.editClinic);
+        addButton.setOnClickListener(new SafeOnClickListener() {
             @Override
             public void onOneClick(View v) {
                 Intent i = new Intent(AddCheckupActivity.this, SearchClinicActivity.class);
@@ -59,16 +50,19 @@ public class AddCheckupActivity extends AppCompatActivity {
 
         Button addCheckButton = findViewById(R.id.addCheckUp);
         addCheckButton.setOnClickListener(new SafeOnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onOneClick(View v) {
-                Toast addCheckupToast = Toast.makeText(AddCheckupActivity.this, R.string.AddCheckupSuccess, Toast.LENGTH_LONG);
-                addCheckupToast.setGravity(Gravity.CENTER,0,0);
-                addCheckupToast.show();
-                Intent i = new Intent(AddCheckupActivity.this, HomeActivity.class);
-                startActivity(i);
+                if (new CheckUpMgr().addCheckUp(AddCheckupActivity.this)) {
+                    Toast addCheckupToast = Toast.makeText(AddCheckupActivity.this, R.string.AddCheckupSuccess, Toast.LENGTH_LONG);
+                    addCheckupToast.show();
+                    finish();
+                }
+                else {
+                    Toast.makeText(AddCheckupActivity.this, R.string.AddCheckupFailure, Toast.LENGTH_LONG);
+                }
             }
         });
-
     }
 
     public void returnToScheduleFrag(View v) {
